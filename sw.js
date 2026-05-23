9afd205721159a067732d062faadc6c8dbda1cb2
alexcm86 <anticipatejh@gmail.com>
2026-05-24 00:07:01 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // v5 - 캐시 완전 초기화
-const CACHE = 'kpi-v5';
+const CACHE = 'kpi-v6';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
