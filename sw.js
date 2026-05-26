5a75a3142ade6c1bac54996628af40e277395e4e
alexcm86 <anticipatejh@gmail.com>
2026-05-26 19:40:38 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// v6 - 디자인 업데이트
-const CACHE = 'kpi-v14';
+// v7 - 아이콘 업데이트
+const CACHE = 'kpi-v15';
 const FILES = ['/alexkpi/', '/alexkpi/index.html', '/alexkpi/manifest.json', '/alexkpi/icon-192.png', '/alexkpi/icon-512.png'];
 
 self.addEventListener('install', e => {
