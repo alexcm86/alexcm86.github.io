6ec57c7f86e57c40cc07ab9fcd1d8d821b0838d9
alexcm86 <anticipatejh@gmail.com>
2026-06-02 18:53:40 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// sw.js v20260602210000
-const CACHE_NAME = 'haenaem-v20260602210000';
+// sw.js v20260602220000
+const CACHE_NAME = 'haenaem-v20260602220000';
 const ASSETS = [
   '/',
   '/index.html',
